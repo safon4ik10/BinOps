02b3a5808924750e50f4b0ef44956c9713b484c6
Vladimir Troshin <vtroshin@rvision.pro>
2022-07-07 17:44:09 +0300

    Добавил конвертацию обратно в двоичную систему

--- src/main/java/BinOps.java
+++ src/main/java/BinOps.java
@@ -3,12 +3,12 @@ public class BinOps {
     public String sum(String a, String b){
         int c = Integer.parseInt(a, 2);
         int d = Integer.parseInt(b, 2);
-        return String.valueOf(c + d);
+        return Integer.toBinaryString(c + d);
     }
 
     public String mult(String a, String b){
         int c = Integer.parseInt(a, 2);
         int d = Integer.parseInt(b, 2);
-        return String.valueOf(c * d);
+        return Integer.toBinaryString(c * d);
     }
 }
